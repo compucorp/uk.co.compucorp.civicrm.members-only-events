9b296d6599efa0f0984d728e3813c3b3160e0d79
Guanhuan Chen <oooomic@gmail.com>
2015-02-12 18:40:00 +0000

    Now contribution status change will trigger the status change of both participant and membership

--- CRM/Contribute/BAO/Contribution.php
+++ CRM/Contribute/BAO/Contribution.php
@@ -1429,11 +1429,27 @@ LEFT JOIN  civicrm_contribution contribution ON ( componentPayment.contribution_
     if (!$baseIPN->validateData($input, $ids, $objects, FALSE)) {
       CRM_Core_Error::fatal();
     }
-
+    
     $memberships   = &$objects['membership'];
     $participant   = &$objects['participant'];
     $pledgePayment = &$objects['pledge_payment'];
     $contribution  = &$objects['contribution'];
+    
+    if($input!=='membership'&&!empty($ids['membership'])&&!is_array($memberships)){
+      $memObjects = array();
+      $input2 = 'membership';
+      $ids2['contribution'] = $contributionId;
+      $ids2['contact'] = CRM_Utils_Array::value('contact_id', $componentDetails);
+      $ids2['membership'] = CRM_Utils_Array::value('membership', $componentDetails);
+      $ids2['pledge_payment'] = CRM_Utils_Array::value('pledge_payment', $componentDetails);
+      $ids2['contributionRecur'] = NULL;
+      $ids2['contributionPage'] = NULL;
+      if(!$baseIPN->validateData($input2, $ids2, $memObjects, FALSE)){
+        CRM_Core_Error::fatal();
+      }
+      $objects['membership'] = $objects2['membership'];
+      $memberships = &$objects['membership'];
+    }
 
     if ($pledgePayment) {
       $pledgePaymentIDs = array();
@@ -2420,8 +2436,12 @@ WHERE  contribution_id = %1 ";
 
 
     $template->assign('trxn_id', $this->trxn_id);
+    //guanhuan
+    $correct_date = str_replace(' ', '', $this->receive_date);
+    $correct_date = str_replace('-', '', $correct_date);
+    $correct_date = str_replace(':', '', $correct_date);
     $template->assign('receive_date',
-      CRM_Utils_Date::mysqlToIso($this->receive_date)
+      CRM_Utils_Date::mysqlToIso($correct_date)
     );
     $template->assign('contributeMode', 'notify');
     $template->assign('action', $this->is_test ? 1024 : 1);
